37f34b63b25132142c7f4acf4873bf7786dd1b9a
Alex Grozav <alex@grozav.com>
2022-03-06 19:15:05 +0200

    feat: Updated react computed to memoize the result based on deps.

--- src/react/computed.ts
+++ src/react/computed.ts
@@ -1,9 +1,12 @@
 import { Ref } from '@inkline/paper/types';
+import { useMemo } from 'react';
 
-export function computed<T> (computeFn: () => T): Ref<T> {
+export function computed<T> (computeFn: () => T, dependencies?: any[]): Ref<T> {
+    const value = useMemo(() => computeFn(), dependencies);
+
     return {
         get value () {
-            return computeFn();
+            return value;
         }
     };
 }
