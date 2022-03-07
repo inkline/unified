0ae7171a5a907a21e4347238396d445d945651ae
Alex Grozav <alex@grozav.com>
2022-03-07 14:45:23 +0200

    feat: Added build config for common package

--- packages/react/src/h.ts
+++ packages/react/src/h.ts
@@ -1,8 +1,8 @@
-import React, { FC } from 'react';
+import { createElement, FC } from 'react';
 import { VNode, HoistFn } from './types';
 
 export const h: HoistFn<VNode, FC<any>> = (
     type,
     props?,
     children?
-) => React.createElement(type, props, children);
+) => createElement(type, props, children);
