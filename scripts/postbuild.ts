d4d9e22b663246477d2d5512581555cb197a5944
Alex Grozav <alex@grozav.com>
2022-03-08 17:05:15 +0200

    fix: Package building now correctly merging files.

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -12,16 +12,17 @@ shell.cd(resolve(__dirname, '..'));
  */
 
 shell.mkdir('lib');
-shell.mv('packages/common/lib/cjs/*', 'lib');
-shell.mv('packages/common/lib/mjs/*', 'lib');
+shell.cp('-r', 'packages/common/lib/cjs/*', 'lib');
+shell.cp('-r', 'packages/common/lib/mjs/*', 'lib');
 
 [
     'react',
     'vue'
 ].forEach((packageName) => {
     shell.mkdir(`lib/${packageName}`);
-    shell.mv(`packages/${packageName}/lib/cjs/*`, `lib/${packageName}`);
-    shell.mv(`packages/${packageName}/lib/mjs/*`, `lib/${packageName}`);
+    shell.cp('-r', `packages/${packageName}/lib/cjs/*`, `lib/${packageName}`);
+    shell.cp('-r', `packages/${packageName}/lib/mjs/*`, `lib/${packageName}`);
+    shell.rm('-r', `packages/${packageName}/lib`);
 });
 
 shell.rm('-r', 'lib/**/__tests__');
