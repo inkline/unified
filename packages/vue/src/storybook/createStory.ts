fc66e9b7a886d96d9005b2634786fa8512329fda
Alex Grozav <alex@grozav.com>
2022-03-18 11:49:31 +0200

    fix: Updated vue createStory expected type

--- packages/vue/src/storybook/createStory.ts
+++ packages/vue/src/storybook/createStory.ts
@@ -1,7 +1,7 @@
 import { CreateStoryFn, defineComponent, h } from '../index';
 import { DefineComponent } from '@vue/runtime-core';
 
-export const createStory: CreateStoryFn<DefineComponent> = (component, storyArgs = {}) => (args: Record<string, any>) => defineComponent({
+export const createStory: CreateStoryFn<DefineComponent<any>> = (component, storyArgs = {}) => (args: Record<string, any>) => defineComponent({
     render: () => h('div', {
         class: `storybook-example ${storyArgs.class || ''}`
     }, h(component, args))
